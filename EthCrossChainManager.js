d7cabb526422c739c3cfa6d3250f7ad3ea0f4f25
Jean-Loïc Mugnier <jeanloicmugnier@hotmail.com>
2022-06-06 17:23:19 +0200

    more editing to display

--- EthCrossChainManager.js
+++ EthCrossChainManager.js
@@ -2,23 +2,15 @@ contract EthCrossChainManager{
     
     function verifyHeaderAndExecuteTx( PARAMETERS ) 
     whenNotPaused public returns (bool){
-        // [...] Parameters parsing, verifications and validations
-        
+        // [...] 
         _executeCrossChainTx(toContract, OTHER_PARAMETERS);
-        
-        // [...]  verifications and validations and return statement
+        // [...] 
     }
-}
-
-contract EthCrossChainManager{
-
-  function _executeCrossChainTx(address _toContract, OTHER_PARAMETERS) 
-     internal returns (bool){
-        // [...] Verifications and validations
-
+    function _executeCrossChainTx(address _toContract, OTHER_PARAMETERS) 
+    internal returns (bool){
+        // [...] 
         _toContract.call(HASH(METHOD_NAME), OTHER_PARAMETERS);
-        
-        // [...] More Verifications and validations and return statement
+        // [...] 
     }
 }
 
